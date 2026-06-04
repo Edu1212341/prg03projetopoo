d62fd8628a5945316da4d0b10369d188eed947d3
Edu1212341 <167384147+Edu1212341@users.noreply.github.com>
2026-06-04 11:29:00 -0300

    style: Organização dos pacotes

---
+++ prg03sistematributario/src/main/java/br/com/ifba/Application.java
@@ -0,0 +1,19 @@
+package br.com.ifba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+                ConfigurableApplicationContext context = 
+                new SpringApplicationBuilder(Application.class)
+                .headless(false)
+                .run(args);
+	}
+
+}
---
+++ prg03sistematributario/src/test/java/br/com/ifba/prg03sistematributario/Prg03sistematributarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifba.prg03sistematributario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prg03sistematributarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
